fdedd1d999ccb214a908864deb9946a1a45b47cd
Ren De <jer12182002@gmail.com>
2020-05-01 13:59:07 -0400

    set up for necessary resources e.g. $, moment,  scss

--- src/components/pickup/pickupMain.js
+++ src/components/pickup/pickupMain.js
@@ -1,6 +1,12 @@
 import React from 'react';
+import {Link} from 'react-router-dom';
+import Moment from 'moment';
+import $ from 'jquery';
+import "./pickupMain.scss";
 
 export default class pickupMain extends React.Component {
+	intervalName="pickUpOrders-notification";
+
 	constructor(props) {
 		super(props);
 		this.state = {
@@ -27,14 +33,36 @@ export default class pickupMain extends React.Component {
 
 	render() {
 		return (
-			<div className ="pick-wrapper">
+			<div className ="pickup-wrapper">
 				<div className="header-section">Pick up page {this.state.accountInfo.USERNAME}</div>
+				<div className="notification-wrapper"></div>
 				<div className="main-section">
 					<table>
+						<thead>
+							<tr>
+								<td>Index</td>
+								<td>Order Number</td>
+								<td>Customer</td>
+								<td>Time</td>
+								<td>Action</td>
+							</tr>
+						</thead>
 						<tbody>
 							{this.state.PICKUP_ORDERS.map((order,key) =>
 								<tr key={key+1}>
+									<td>{key+1}</td>
+									<td>{order.ORDER_ID}</td>
 									<td>{order.CUSTOMER}</td>
+									<td>{order.ORDER_TIME}</td>
+									<td>
+										<Link className="btn btn-success" to={{
+											pathname: "/pickup/order-detail",
+											state: {
+												orderId: order,
+												accountInfo: this.state.accountInfo
+											}
+										}}>Proceed</Link>
+									</td>
 
 								</tr>
 
